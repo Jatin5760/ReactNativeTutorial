72ec002bbf527e626424fbc75cbc0ecfba3ae458
Jatin5760 <jatinsharma14202003@gmail.com>
2025-07-03 01:48:08 +0530

    Commit

--- src/components/UseStateHook.jsx
+++ src/components/UseStateHook.jsx
@@ -1,13 +1,17 @@
-import { View, Text } from 'react-native'
-import React from 'react'
-
-
-const [count, setCount] = useState();
+import { View, Text, Button } from 'react-native';
+import React, { useState } from 'react';
 
 export default function UseStateHook() {
+  const [name, setName] = useState('Jatin');
+  const updateName = () => {
+    setName('Jayant');
+  };
+
   return (
-    <View>
-      <Text style={{fontSize: 30}}>UseStateHook</Text>
+    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+      <Text style={{ fontSize: 30 }}>UseStateHook</Text>
+      <Text style={{ fontSize: 30 }}>Name: {name}</Text>
+      <Button title="Press" onPress={updateName} />
     </View>
-  )
+  );
 }
