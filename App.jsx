c238b59d36545d145163beb3dec98d128fe1acc1
Jatin5760 <jatinsharma14202003@gmail.com>
2025-07-03 01:01:22 +0530

    Commit

--- App.jsx
+++ App.jsx
@@ -1,17 +1,22 @@
 import { View, Text, Button } from 'react-native'
 import React from 'react'
 //import First from './src/components/First'
-import JSX from "./src/components/JSX"
+//import JSX from "./src/components/JSX"
+import OnPress from './src/components/OnPress'
+
 
 const App = () => {
   return (
     <View style={{flex:1, justifyContent:"center", alignItems:"center"}}>
-      <Text style={{fontSize: 30}}>Jatin Sharma</Text>
+      {/* <Text style={{fontSize: 30}}>Jatin Sharma</Text>
       <Text style={{fontSize: 30}}>I'm a Application Developer</Text>
-      <Button title="Press Me"/>
+      <Button title="Press Me"/> */}
     {/* <First/> */}
 
-    <JSX/>
+    {/* <JSX/> */}
+
+
+    <OnPress/>
     </View>
   )
 }
---
+++ src/components/OnPress.jsx
@@ -0,0 +1,12 @@
+import { View, Text, Button } from 'react-native'
+import React from 'react'
+
+
+export default function OnPress() {
+  return (
+    <View>
+      <Text style = {{fontSize: 30}}>OnPress</Text>
+      <Button/>
+    </View>
+  )
+}
