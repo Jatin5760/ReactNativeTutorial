870f0fc015fed67b4382926f0c619984ba0bc94b
Jatin5760 <jatinsharma14202003@gmail.com>
2025-07-03 01:30:58 +0530

    Commit

--- src/components/OnPress.jsx
+++ src/components/OnPress.jsx
@@ -1,12 +1,15 @@
-import { View, Text, Button } from 'react-native'
-import React from 'react'
+import { View, Text, Button } from 'react-native';
+import React from 'react';
 
+const getName = (name) => {
+  console.log("Name", name);
+};
 
 export default function OnPress() {
   return (
-    <View>
-      <Text style = {{fontSize: 30}}>OnPress</Text>
-      <Button/>
+    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+      <Text style={{ fontSize: 30 }}>OnPress</Text>
+      <Button onPress={() => getName("Jatin")} title="Press" />
     </View>
-  )
+  );
 }
