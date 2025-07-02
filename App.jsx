d59a8cb318d535f33147dddc18f2400c93cd23e5
Jatin5760 <jatinsharma14202003@gmail.com>
2025-07-03 00:02:30 +0530

    Commit

--- App.jsx
+++ App.jsx
@@ -1,14 +1,14 @@
 import { View, Text, Button } from 'react-native'
 import React from 'react'
 //import First from './src/components/First'
-import {JSX} from "./src/components/JSX"
+import JSX from "./src/components/JSX"
 
 const App = () => {
   return (
     <View style={{flex:1, justifyContent:"center", alignItems:"center"}}>
       <Text style={{fontSize: 30}}>Jatin Sharma</Text>
       <Text style={{fontSize: 30}}>I'm a Application Developer</Text>
-      <Button title="Press Here"/>
+      <Button title="Press My boobs"/>
     {/* <First/> */}
 
     <JSX/>
--- src/components/JSX.jsx
+++ src/components/JSX.jsx
@@ -2,11 +2,18 @@ import { View, Text } from 'react-native'
 import React from 'react'
 
 const JSX = () => {
+  let name = "Jatin";
+  const getAge = () => {
+    return 22;
+  }
+}
+export default function JSX () {
   return (
     <View>
-      <Text>JSX</Text>
+      <Text style={{fontSize: 30}}>JSX</Text>
+      <Text style={{fontSize: 30}}>{name}</Text>
+      <Text style={{fontSize: 30}}>{5+3}</Text>
+      <Text style={{fontSize: 30}}>{getAge()}</Text>
     </View>
   )
 }
-
-export default JSX
