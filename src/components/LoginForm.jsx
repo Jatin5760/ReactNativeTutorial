fcb5b6ea073128cdebde761caf8ee5dd45848133
Jatin5760 <jatinsharma14202003@gmail.com>
2025-07-03 23:52:15 +0530

    Commit

--- src/components/LoginForm.jsx
+++ src/components/LoginForm.jsx
@@ -1,12 +1,92 @@
-import { View, Text } from 'react-native'
-import React, { useState } from 'react'
+import { View, Text, TextInput, TouchableOpacity, Button, StyleSheet } from 'react-native';
+import React, { useState } from 'react';
 
 export default function LoginForm() {
-    const [email, setEmail] = useState()
-    const [pass, setPassword] = useState()
+  const [email, setEmail] = useState('');
+  const [pass, setPassword] = useState('');
+  const handleLogin = ()=>{
+    console.log('Email:', email)
+    console.log('Email:', pass)
+
+    setEmail('')
+    setPassword('')
+  }
   return (
-    <View>
-      <Text>LoginForm</Text>
+    <View style={styles.container}>
+      <Text style={styles.heading}>LoginForm</Text>
+      <TextInput
+      style={styles.input}
+        placeholder="Email"
+        value={email}
+        onChangeText={setEmail}
+        keyboardType="email-address"
+        autoCapitalize="none"
+        placeholderTextColor="#999"
+      />
+
+      <TextInput
+      style={styles.input}
+        placeholder="Password"
+        value={pass}
+        onChangeText={setPassword}
+        secureTextEntry
+        placeholderTextColor="#999"
+      />
+
+      <TouchableOpacity onPress={handleLogin} style={styles.buttonStyle}>
+        <Text style={styles.btnText}>Login</Text>
+      </TouchableOpacity>
     </View>
-  )
+  );
 }
+
+const styles = StyleSheet.create({
+    container:{
+        justifyContent:'center',
+        paddingHorizontal:20,
+        paddingTop: 40,
+        backgroundColor: '#f0f0f0',
+    },
+    heading:{
+        fontSize: 20,
+        fontWeight:'bold',
+        marginBottom: 30,
+        textAlign:'center',
+        color:"#333"
+    },
+    input:{
+        height:50,
+        borderColor:"#f0f0f0",
+        borderWidth:1,
+        marginBottom: 20,
+        paddingHorizontal: 15,
+        borderRadius:10,
+        backgroundColor:"#fff",
+        fontSize:16,
+        color:"#333"
+    },
+    password:{
+        height:50,
+        borderColor:"#f0f0f0",
+        borderWidth:1,
+        marginBottom: 20,
+        paddingHorizontal: 15,
+        borderRadius:10,
+        backgroundColor:"#fff",
+        fontSize:16,
+        color:"#333"
+    },
+    buttonStyle:{
+        backgroundColor:'#6200EE',
+        paddingVertical:15,
+        borderRadius:10,
+        alignItems:'center'
+    },
+    btnText:{
+        color:"#fff",
+        fontSize:18,
+        fontWeight:"bold"
+    }
+
+
+})
