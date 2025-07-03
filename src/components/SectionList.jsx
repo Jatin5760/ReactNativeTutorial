b251c844ab95a95f2ac5a1743c570838e50ed4c7
Jatin5760 <jatinsharma14202003@gmail.com>
2025-07-03 16:46:18 +0530

    Commit

--- src/components/SectionList.jsx
+++ src/components/SectionList.jsx
@@ -1,10 +1,40 @@
-import { View, Text } from 'react-native'
-import React from 'react'
+import { View, Text, SectionList } from 'react-native';
+import React from 'react';
 
-export default function SectionList() {
+export default function MySectionList() {
+  const listItem = [
+    {
+      title: 'Fruits',
+      data: [
+        { id: 1, name: 'Apple' },
+        { id: 2, name: 'Banana' },
+        { id: 3, name: 'Cherry' },
+        { id: 4, name: 'Mango' },
+      ],
+    },
+    {
+      title: 'Vegetables',
+      data: [
+        { id: 1, name: 'Carrot' },
+        { id: 2, name: 'Potato' },
+        { id: 3, name: 'Tomato' },
+        { id: 4, name: 'Onion' },
+      ],
+    },
+  ];
+
+  const renderItem = ({item}) => (
+    <View>
+        <Text>{item.name}</Text>
+    </View>
+  );
+  
   return (
     <View>
-      <Text>SectionList Jatin</Text>
+      <SectionList
+        sections={listItem}
+        renderItem={renderItem}
+      />
     </View>
-  )
+  );
 }
