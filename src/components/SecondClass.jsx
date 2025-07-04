e666b3f9a0e856fc43260eadbe4df73dd9220a8c
Jatin5760 <jatinsharma14202003@gmail.com>
2025-07-05 00:31:13 +0530

    Commit

--- src/components/SecondClass.jsx
+++ src/components/SecondClass.jsx
@@ -1,18 +1,24 @@
 import React, { Component } from 'react'
-import { Text, View } from 'react-native'
+import { Button, Text, View } from 'react-native'
 
 export default class SecondClass extends Component {
+    // Ye ek special method hai class ka, koi bhi component render hone se phele ye method call hota hai.
     constructor(){
+        // Super bascially jo parent class ka constructor hota hai usko automatically call kardeta  hai.
         super();
         this.state = {
             myName : 'Jatin',
         };
     }
-    
+    updateName = () => {
+        this.setState({myName: 'Jayant'})
+    }
+
   render() {
     return (
       <View>
-        <Text style={{fontSize:20}}> textInComponent </Text>
+        <Text style={{fontSize:20}}>Name: {this.state.myName} </Text>
+        <Button title='Update name' onPress={this.updateName}/>
       </View>
     )
   }
