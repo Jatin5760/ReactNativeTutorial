10fc065638bd5da3f1d70bf54a901c9694ab56bf
Jatin5760 <jatinsharma14202003@gmail.com>
2025-07-05 23:30:22 +0530

    Commit

--- src/components/UseEffectHookUpdatingPhase.jsx
+++ src/components/UseEffectHookUpdatingPhase.jsx
@@ -1,6 +1,20 @@
 import { View, Text, Button } from 'react-native'
 import React, { useEffect, useState } from 'react'
 
+const InfoDetails = ({count,points}) => {
+    useEffect(() => {
+      console.log("I'm a child component");
+    }, [])
+    
+    return (
+        <View>
+            <Text style={{fontSize: 30, marginBottom: 10}}>Info Details</Text>
+            <Text style={{fontSize: 30, marginBottom: 10}}>count: {count}</Text>
+            <Text style={{fontSize: 30, marginBottom: 10}}>points: {points}</Text>
+        </View>
+    );
+}
+
 export default function UseEffectHookUpdatingPhase() {
     const [counter, setCounter] = useState(0);
     const [score, setScore] = useState(20);
@@ -13,14 +27,14 @@ export default function UseEffectHookUpdatingPhase() {
         console.log("Useffect is called by updating score");
         
     }, [score]);
-    
   return (
     <View>
       <Text style={{fontSize: 30, marginBottom: 10}}>UseEffectHookUpdatingPhase</Text>
       <Text style={{fontSize: 30, marginBottom: 10}}>Counter: {counter}</Text>      
       <Text style={{fontSize: 30, marginBottom: 10}}>Score: {score}</Text> 
       <Button title='Counter' onPress={()=> setCounter(counter+ 1)}/>     
-      <Button title='Score' onPress={()=> setScore(score + 1)}/>     
+      <Button title='Score' onPress={()=> setScore(score + 1)}/>   
+      <InfoDetails count={counter} points={score}/>  
     </View>
   )
 }
