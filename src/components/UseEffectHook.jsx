47972941f5f58f4fe42a196a31c1b2901921d03e
Jatin5760 <jatinsharma14202003@gmail.com>
2025-07-05 11:18:31 +0530

    Commit

--- src/components/UseEffectHook.jsx
+++ src/components/UseEffectHook.jsx
@@ -1,5 +1,5 @@
-import { View, Text } from 'react-native'
-import React, { useEffect } from 'react'
+import { View, Text, Button } from 'react-native'
+import React, { useEffect, useState } from 'react'
 
 export default function UseEffectHook() {
 
@@ -7,14 +7,17 @@ export default function UseEffectHook() {
     // Mouting, Updating and unmounting
     // API se data fetch karte time Useeffect ka use karte hai,
     // Manlo gar useeffect ka use nahi kiya aur api se data fetch karliya, aur agar ui ke render hone se pehele data api se nahi aya toh baat bigad sakti hai.
+    const [count, setCount] = useState(1);
     useEffect(() => {
         // Mounting lifecycle use hua jaise ji component render hua ye run ho jayega
-      console.log("This is a useEffect Hook");
+        console.log("This is a useEffect Hook");
     }, [])
     
   return (
     <View>
       <Text style={{fontSize: 30}}>UseEffectHook</Text>
+      <Text style={{fontSize: 30}}>Count:{count}</Text>
+      <Button title='Counter' onPress={()=> setCount(count + 1)}/>
     </View>
   )
 }
