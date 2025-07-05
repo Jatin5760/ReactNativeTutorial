26078aaee382c6ab32036ed01662a46de173eaf1
Jatin5760 <jatinsharma14202003@gmail.com>
2025-07-05 11:06:54 +0530

    Commit

--- src/components/UseEffectHook.jsx
+++ src/components/UseEffectHook.jsx
@@ -4,7 +4,7 @@ import React, { useEffect } from 'react'
 export default function UseEffectHook() {
 
     // UseEffect se ham log funtion ki lifecycle method check kar sakte hai
-    // Mouting
+    // Mouting, Updating and unmounting
     // API se data fetch karte time Useeffect ka use karte hai,
     // Manlo gar useeffect ka use nahi kiya aur api se data fetch karliya, aur agar ui ke render hone se pehele data api se nahi aya toh baat bigad sakti hai.
     useEffect(() => {
