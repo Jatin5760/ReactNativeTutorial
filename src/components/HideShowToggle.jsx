b75a57214608a82a943c95986205117348889c85
Jatin5760 <jatinsharma14202003@gmail.com>
2025-07-06 19:28:48 +0530

    Commit

--- src/components/HideShowToggle.jsx
+++ src/components/HideShowToggle.jsx
@@ -1,10 +1,18 @@
-import { View, Text } from 'react-native'
-import React from 'react'
+import { View, Text, Button } from 'react-native'
+import React, { useState } from 'react'
+import My from './My';
 
 export default function HideShowToggle() {
+    const [status, setStatus] = useState(true);
   return (
     <View>
-      <Text>HideShowToggle</Text>
+      <Text style={{fontSize: 30}}>HideShowToggle</Text>
+      {
+        status ? <My /> : null
+      }
+      <Button title='Hide' onPress={()=> setStatus(false)}/>
+      <Button title='Show' onPress={()=> setStatus(true)}/>
+      <Button title='Toggle' onPress={()=> setStatus(!status)}/>
     </View>
   )
 }
---
+++ src/components/My.jsx
@@ -0,0 +1,10 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+
+export default function My() {
+  return (
+    <View>
+      <Text style={{fontSize: 30}}>My</Text>
+    </View>
+  )
+}
